a6a163bbf06d098fe42a0cbd90c7946e219176fc
Harsha <120081960+HARSHANAIKA@users.noreply.github.com>
2024-04-08 20:02:08 +0530

    some changes in calculator

--- script.js
+++ script.js
@@ -8,9 +8,28 @@ function clearDisplay() {
 
 function calculateResult() {
     try {
-        const result = eval(document.getElementById('display').value);
-        document.getElementById('display').value = result;
+        const expression = document.getElementById('display').value;
+        if (/^[0-9+*/.\-() ]+$/.test(expression)) {
+            const result = eval(expression);
+            document.getElementById('display').value = result;
+        } else {
+            throw new Error('Invalid input');
+        }
     } catch (error) {
-        document.getElementById('display').value = 'Error';
+        document.getElementById('display').value = 'ERROR';
     }
+}
+
+function isValidKey(event) {
+    const key = event.key;
+    return /[0-9+\-*/.%()=Enter]/.test(key);
+}
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'Enter') {
+        calculateResult();
+    }
+});
+
+function focusInputField() {
+    document.getElementById('display').focus();
 }
